33f9edb0ebcc8908d31deaddb4246d38209bc901
Kazunori Sato <kazunori279@gmail.com>
2017-08-30 00:19:06 +0000

    modified and retrained the model

--- toilet/predict.py
+++ toilet/predict.py
@@ -2,12 +2,10 @@
 # predict.py
 #
 
-import os
-import sys
-from keras.applications.vgg16 import VGG16
-from keras.models import Sequential, Model
-from keras.layers import Input, Activation, Dropout, Flatten, Dense
+from keras import applications, optimizers
 from keras.preprocessing import image
+from keras.models import Sequential, Model
+from keras.layers import Dropout, Flatten, Dense
 import numpy as np
 
 filename = 'toilet.jpg'
@@ -18,26 +16,24 @@ channels = 3
 model = None
 
 def init_models():
+  global model
 
-  # VGG16
-  input_tensor = Input(shape=(img_height, img_width, channels))
-  vgg16_model = VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor)
+  # build the VGG16 network
+  base_model = applications.VGG16(weights='imagenet', include_top=False, input_shape=(150,150,3))
 
-  # top model 
+  # build a classifier model to put on top of the convolutional model
   top_model = Sequential()
-  top_model.add(Flatten(input_shape=vgg16_model.output_shape[1:]))
+  top_model.add(Flatten(input_shape=base_model.output_shape[1:]))
   top_model.add(Dense(256, activation='relu'))
   top_model.add(Dropout(0.5))
   top_model.add(Dense(1, activation='sigmoid'))
-  top_model.load_weights('bottleneck_fc_model.h5')
 
-  # final model 
-  global model
-  model = Model(input=vgg16_model.input, output=top_model(vgg16_model.output))
+  # full model
+  model = Model(inputs=base_model.input, outputs=top_model(base_model.output))
   model.compile(loss='binary_crossentropy',
-              optimizer='adam',
+              optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
               metrics=['accuracy'])
-  #model.summary()
+  model.load_weights('full_model.h5')
 
 # predict
 def predict():
@@ -45,5 +41,5 @@ def predict():
   x = image.img_to_array(img)
   x = np.expand_dims(x, axis=0)
   x = x / 255.0
-  return model.predict(x)[0] > 0.5
+  return model.predict(x)[0]
 
--- toilet/train.py
+++ toilet/train.py
@@ -1,82 +1,85 @@
-#
-# train.py
-#
-
-import numpy as np
-from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Dropout, Flatten, Dense
 from keras import applications
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+from keras.models import Sequential
+from keras.models import Model
+from keras.layers import Dropout, Flatten, Dense
 
-# dimensions of our images.
-img_width, img_height = 150, 150
-
+# path to the model weights files.
+#weights_path = '../keras/examples/vgg16_weights.h5'
 top_model_weights_path = 'bottleneck_fc_model.h5'
+img_width, img_height = 150, 150 
+
 train_data_dir = 'data/train'
 validation_data_dir = 'data/validation'
-nb_train_dry = 364
-nb_train_wet = 111
-nb_train_samples = nb_train_dry + nb_train_wet
-nb_validation_dry = 41
-nb_validation_wet = 11 
-nb_validation_samples = nb_validation_dry + nb_validation_wet 
-epochs = 50
-batch_size = 1
+nb_train_samples = 475 
+nb_validation_samples = 52 
+epochs = 500
+batch_size = 50 
 
-# apply VGG16 on the data, save bottleneck features
-def save_bottlebeck_features():
-    datagen = ImageDataGenerator(rescale=1. / 255)
+# build the VGG16 network
+base_model = applications.VGG16(weights='imagenet', include_top=False, input_shape=(150,150,3))
+print('Model loaded.')
 
-    # build the VGG16 network
-    model = applications.VGG16(include_top=False, weights='imagenet')
+# build a classifier model to put on top of the convolutional model
+top_model = Sequential()
+top_model.add(Flatten(input_shape=base_model.output_shape[1:]))
+top_model.add(Dense(256, activation='relu'))
+top_model.add(Dropout(0.5))
+top_model.add(Dense(1, activation='sigmoid'))
 
-    generator = datagen.flow_from_directory(
-        train_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode=None,
-        shuffle=False)
-    bottleneck_features_train = model.predict_generator(
-        generator, nb_train_samples // batch_size)
-    np.save(open('bottleneck_features_train.npy', 'w'),
-            bottleneck_features_train)
+# note that it is necessary to start with a fully-trained
+# classifier, including the top classifier,
+# in order to successfully do fine-tuning
+top_model.load_weights(top_model_weights_path)
 
-    generator = datagen.flow_from_directory(
-        validation_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode=None,
-        shuffle=False)
-    bottleneck_features_validation = model.predict_generator(
-        generator, nb_validation_samples // batch_size)
-    np.save(open('bottleneck_features_validation.npy', 'w'),
-            bottleneck_features_validation)
+# add the model on top of the convolutional base
+# model.add(top_model)
+model = Model(inputs=base_model.input, outputs=top_model(base_model.output))
 
-# train a NN model with the bottleneck features
-def train_top_model():
-    train_data = np.load(open('bottleneck_features_train.npy'))
-    train_labels = np.array(
-        [0] * nb_train_dry + [1] * nb_train_wet)
+# set the first 25 layers (up to the last conv block)
+# to non-trainable (weights will not be updated)
+for layer in model.layers[:15]:
+    layer.trainable = False
 
-    validation_data = np.load(open('bottleneck_features_validation.npy'))
-    validation_labels = np.array(
-        [0] * nb_validation_dry + [1] * nb_validation_wet)
+# compile the model with a SGD/momentum optimizer
+# and a very slow learning rate.
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+              metrics=['accuracy'])
 
-    model = Sequential()
-    model.add(Flatten(input_shape=train_data.shape[1:]))
-    model.add(Dense(256, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1, activation='sigmoid'))
+# prepare data augmentation configuration
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
 
-    model.compile(optimizer='rmsprop',
-                  loss='binary_crossentropy', metrics=['accuracy'])
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-    model.fit(train_data, train_labels,
-              epochs=epochs,
-              batch_size=batch_size,
-              validation_data=(validation_data, validation_labels))
-    model.save_weights(top_model_weights_path)
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='binary')
 
-# main
-save_bottlebeck_features()
-train_top_model()
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_height, img_width),
+    batch_size=batch_size,
+    class_mode='binary')
+
+model.summary()
+
+# fine-tune the model
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size,
+    verbose=2)
+
+model.save_weights('full_model.h5')
+print('Model saved.')
+
